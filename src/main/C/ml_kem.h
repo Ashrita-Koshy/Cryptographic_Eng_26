a97bb752c67f6916e42aef8a2a0ddf2e5c9e78e3
Nolan Attreed <ntattreed@vt.edu>
2026-04-08 16:11:24 -0400

    pushing initial PKE code

--- src/main/C/ml_kem.h
+++ src/main/C/ml_kem.h
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <string.h>
 #include "fips202.h"
 
 #define MLKEM_Q 3329
@@ -9,6 +10,7 @@
 #define MLKEM_ETA 2
 #define D_U 11
 #define D_UPSILON 5
+#define CBD_SEED_LEN 64*MLKEM_ETA
 
 /* Precomputed Zeta powers as defined in FIPS 203 Appendix A */
 static const int16_t zetaPowers[128] = {
@@ -50,15 +52,13 @@ static const int16_t zetaOddPowers[128] = {
     2110, -2110, 2935, -2935,  885,  -885, 2154, -2154
 };
 
-#define XOF_ctx                   keccak_state
-#define XOF_Init(a)                shake128_init(a)
-#define XOF_Absorb(a, b, c)        shake128_absorb(a, b, c)
-#define XOF_Finalize(a)            shake128_finalize(a)
-#define XOF_Squeeze(a, b, c)       shake128_squeeze(b, c, a)
-
-/*These function likely could pass the result as a pointer tbh*/
-uint8_t* bitsToBytes(const uint8_t* bits, size_t l);
-uint8_t* bytesToBits(const uint8_t* bytes, size_t l);
+#define XOF_ctx               keccak_state
+#define XOF_Init(a)           shake128_init(a)
+#define XOF_Absorb(a, b, c)   shake128_absorb(a, b, c)
+#define XOF_Finalize(a)       shake128_finalize(a)
+#define XOF_Squeeze(a, b, c)  shake128_squeeze(b, c, a)
+#define G(a, b, c)            sha3_512(a, b, c)
+#define PRF(a, b, c, d)       shake256(a, b, c, d)
 
 /*These can probably be inlined since d is almost always going to be 12 or 1*/
 uint16_t compress(uint16_t x, uint8_t d);
@@ -66,11 +66,17 @@ uint16_t decompress(uint16_t y, uint8_t d);
 
 /*Pretty sure these are only used with d = 12, so could maybe unroll inner for loop, might not be worth it though*/
 /*Also worth noting, since d is probably oonly going to equal 12, can probably get rid of any memory allocation with these bits/bytes related functions*/
-uint8_t* byteEncode(const uint16_t* F, uint8_t d);
-uint16_t* byteDecode(const uint8_t* B, uint8_t d);
+void byteEncode(uint8_t* B, const uint16_t* F, uint8_t d);
+void byteDecode(uint16_t* F, const uint8_t* B, uint8_t d);
 
-uint16_t* sampleNTT(const uint8_t* B);
-uint16_t* samplePolyCBD(const uint8_t* B);
+void sampleNTT(uint16_t* a, const uint8_t* B, const uint8_t col, const uint8_t row);
+void samplePolyCBD(uint16_t* f, const uint8_t* B);
 
-void NTT(uint16_t* f);
-void NTTInverse(uint16_t* f);
+void ntt(uint16_t* f);
+void nttInverse(uint16_t* f);
+
+void multiplyNTT(uint16_t* h, const uint16_t* f, const uint16_t* g);
+
+void K_PKE_KeyGen(uint8_t* ekPKE, uint8_t* dkPKE, const uint8_t* d);
+void K_PKE_Encrypt(uint8_t* c, const uint8_t* ekPKE, const uint8_t* m, const uint8_t* r);
+void K_PKE_Decrypt(uint8_t* m, const uint8_t* dkPKE, const uint8_t* c);
